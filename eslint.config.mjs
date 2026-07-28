031c0c9d67465d688e3e8d289e2f444af6dc14d7
Reforestal eG <tech@terre-utopia.com>
2026-07-28 19:04:55 -0300

    fix1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
             ecmaVersion: 'latest',
             sourceType: 'module',
             parserOptions: { ecmaFeatures: { jsx: true } },
-            globals: { ...globals.browser, React: 'readonly', Intl: 'readonly' },
+            globals: { ...globals.browser, React: 'readonly', Intl: 'readonly', process: 'readonly', describe: 'readonly', test: 'readonly', expect: 'readonly' },
         },
         settings: {
             react: { version: 'detect' },
@@ -28,6 +28,7 @@ export default [
             'import/no-named-as-default': 'off',
             'import/no-named-as-default-member': 'off',
             
+            'react/no-unknown-property': ['error', { ignore: ['cmdk-input-wrapper'] }],
             'react/prop-types': 'off',
             'react/no-unescaped-entities': 'off',
             'react/react-in-jsx-scope': 'off',
